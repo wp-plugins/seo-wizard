07bbb8b10da1a0e40968b7760a0f3a90632e136f
Seo UK Team <Seo UK Team@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-02 09:08:22 +0000

    Image removed, replaced with text.
    git-svn-id: https://plugins.svn.wordpress.org/seo-wizard/trunk@1080720 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/wsw-show.php
+++ classes/wsw-show.php
@@ -169,7 +169,7 @@ if ( ! class_exists( 'WSW_Show' ) ) {
                 //$settings = get_post_meta( $post_id , 'wsw-settings');
 
                 if (WSW_Main::$settings['chk_author_linking'] == '1') {
-                     echo '<div style="text-align: center;">This site is using the Seo Wizard plugin developed by <a href="http://seo.uk.net"><img src="http://seo.uk.net/wp-content/uploads/2013/11/seo.uk_.net_.gif" width="70" height="20" alt="http://seo.uk.net" /></a></div>' . "\n";
+                     echo '<div style="text-align: center;">This site is using the Seo Wizard plugin powered by <a href="http://seo.uk.net/" title="Seo Uk">http://seo.uk.net/</a></div>' . "\n";
                 }
             }
         }
